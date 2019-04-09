af92a3b3dc1911c997e484532a23d7108c1b05b2
Kushal <kushal.shit@geotechinfo.net>
2019-04-09 19:29:59 +0530

    code init

---
+++ app.js
@@ -0,0 +1,27 @@
+var WebSocketServer = require('ws').Server;
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').createServer();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+var wss = new WebSocketServer({ server: server });
+wss.on('connection', function(ws) { 
+  var id = setInterval(function() {
+    let data = new Date() 
+    ws.send(JSON.stringify(data), function() {
+      /* ignore errors */
+    });
+  }, 1000);
+  console.log('started client interval');
+  ws.on('close', function() {
+    console.log('stopping client interval');
+    clearInterval(id);
+  });
+});
+
+server.on('request', app);
+server.listen(8080, function() {
+  console.log('Listening on http://localhost:8080');
+});
