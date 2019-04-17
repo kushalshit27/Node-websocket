0766dd8b715ce8fc0a7074b2460d9d1e5291a779
Kushal <kushal.shit@geotechinfo.net>
2019-04-17 12:45:58 +0530

    code update

--- app.js
+++ app.js
@@ -15,21 +15,21 @@ var wss = new WebSocketServer({ server: server });
 //const BLOCKCHAIN_URL = 'https://api.blockchair.com/ethereum/blocks?limit=10';
 
 // production 
-const BLOCKCHAIN_URL = 'http://api.localhost:8000/chain/blocks?limit=10';
+//const BLOCKCHAIN_URL = 'http://api.localhost:8000/chain/blocks?limit=10';
 
-wss.on('connection', function(ws) { 
+//development
+const BLOCKCHAIN_URL = 'http://172.18.1.0:22000';
+
+wss.on('connection', function(ws) {
+
   var id = setInterval(function() {
-    
     var data = {}
     data.dateTime = new Date() 
     data.blockchain = null;
 
-    axios.get(BLOCKCHAIN_URL)
+    getNumberBlocks()
     .then(response => {
-      //console.log(response.data);
-      data.blockchain = response.data
-      
-      // push data to client
+      data.blockchain = response.data.result;
       ws.send(JSON.stringify(data), function() {
         /* ignore errors */
       });
@@ -39,14 +39,46 @@ wss.on('connection', function(ws) {
     });
 
   }, 10000);
+
   console.log('started client interval');
+
   ws.on('close', function() {
     console.log('stopping client interval');
     clearInterval(id);
   });
+
 });
 
 server.on('request', app);
 server.listen(8080, function() {
   console.log('Listening on http://localhost:8080');
 });
+
+
+function getNumberBlocks(){
+  return axios({
+    method: 'post',
+    url: BLOCKCHAIN_URL,
+    data: {
+      'jsonrpc': '2.0',
+      'method': 'eth_blockNumber',
+      'params': [],
+      'id':1
+    }
+  });
+  
+}
+
+
+function getBlocksInfo(){
+  return axios({
+    method: 'post',
+    url: BLOCKCHAIN_URL,
+    data: {
+      'jsonrpc': '2.0',
+      'method': 'eth_getBlockByNumber',
+      'params': ["0x1c",true],
+      'id':1
+    }
+  });
+}
